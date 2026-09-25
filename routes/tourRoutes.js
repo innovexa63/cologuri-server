8f0d1b7a37bf82bf3872f1144e30ea82e7c15e38
nbr233 <nurunnabireachad233@gmail.com>
2026-09-25 14:28:30 +0600

    feat: implement booking system and api routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { getTours, getTourById } from '../controllers/tourController.js';
+import {
+  getTours,
+  getTourById,
+  createTour,
+  transferSeat,
+  getJointTourMonitoring,
+} from '../controllers/tourController.js';
 
 const router = express.Router();
 
 router.get('/', getTours);
+router.post('/', createTour);
 router.get('/:id', getTourById);
+router.post('/:id/transfer-seat', transferSeat);
+router.get('/:id/joint-monitor', getJointTourMonitoring);
 
 export default router;
